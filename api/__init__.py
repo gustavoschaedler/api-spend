7cde621fd0ffd3fa0606734d137c2eee04419b92
GMS <gustavopoa@GMS-MacBookPro.local>
2019-04-13 19:43:14 +0100

    put __init__.py data into app.py file

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,2 @@
-from flask_restful import Api
-from app import myApp
-
-from .Root import Root
-from .RootApi import RootApi
-
-from .Spend import Spend
-from .Devs import Devs
-
-restServer = Api(myApp)
-
-restServer.add_resource(Root, "/")
-restServer.add_resource(RootApi, "/api")
-restServer.add_resource(Spend, "/api/spend")
-restServer.add_resource(Devs, "/api/devs")
+#from flask_restful import Api
+#from app import myApp
--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_restful import Api
 import logging as logger
+
+from api.Root import Root
+from api.RootApi import RootApi
+from api.Spend import Spend
+from api.Devs import Devs
 
 logger.basicConfig(level="DEBUG")
 
 myApp = Flask(__name__)
 
+restServer = Api(myApp)
+
+restServer.add_resource(Root, "/")
+restServer.add_resource(RootApi, "/api")
+restServer.add_resource(Spend, "/api/spend")
+restServer.add_resource(Devs, "/api/devs")
+
 if __name__ == "__main__":
     logger.debug("Starting the application")
-    from api import myApp
+    #from api import myApp
 
     myApp.run(debug=True)
