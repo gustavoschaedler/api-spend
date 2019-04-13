25d27d18bb5addfefc3272c40284c0c6890c7616
GMS <gustavopoa@GMS-MacBookPro.local>
2019-04-13 19:23:53 +0100

    renamed app name + Procfile

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,5 @@
 from flask_restful import Api
-from app import flaskAppInstance
+from app import myApp
 
 from .Root import Root
 from .RootApi import RootApi
@@ -7,7 +7,7 @@ from .RootApi import RootApi
 from .Spend import Spend
 from .Devs import Devs
 
-restServer = Api(flaskAppInstance)
+restServer = Api(myApp)
 
 restServer.add_resource(Root, "/")
 restServer.add_resource(RootApi, "/api")
--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import logging as logger
 
 logger.basicConfig(level="DEBUG")
 
-flaskAppInstance = Flask(__name__)
+myApp = Flask(__name__)
 
 if __name__ == "__main__":
-    from api import flaskAppInstance
+    from api import myApp
 
     logger.debug("Starting the application")
 
-    flaskAppInstance.run(debug=True)
+    myApp.run(debug=True)
